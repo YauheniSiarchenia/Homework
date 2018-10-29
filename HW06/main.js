5ce1e787d71a01fd663263d9ce687709d4edb1f6
Yauheni Siarchenia <bizcon24@gmail.com>
2018-10-29 20:12:19 +0300

    Homework 06 - main.js

--- HW06/main.js
+++ HW06/main.js
@@ -7,12 +7,12 @@ var array1 = [123, 'string', true, {value: 321}, null],
 var indexR = 0;
 
 for (var index1 = 0; index1 < array1.length; index1++) {
-    result [indexR] = array1 [index1];
+    result[indexR] = array1[index1];
     indexR++;
 }
 
 for (var index2 = 0; index2 < array2.length; index2++) {
-    result [indexR] = array2 [index2];
+    result[indexR] = array2[index2];
     indexR++;
 }
 

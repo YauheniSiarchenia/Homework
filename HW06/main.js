ab11e4eb59119b7c2e6f6be9f56c20ee629e2b61
Yauheni Siarchenia <bizcon24@gmail.com>
2018-10-29 11:15:53 +0300

    Homework 06 - main.js

--- HW06/main.js
+++ HW06/main.js
@@ -4,6 +4,16 @@ var array1 = [123, 'string', true, {value: 321}, null],
     array2 = ['first','second', 'last'],
     result = [];
 
-result = result.concat (array1, array2);
+var indexR = 0;
+
+for (var index1 = 0; index1 < array1.length; index1++) {
+    result [indexR] = array1 [index1];
+    indexR++;
+}
+
+for (var index2 = 0; index2 < array2.length; index2++) {
+    result [indexR] = array2 [index2];
+    indexR++;
+}
 
 console.log('result: ', result);
